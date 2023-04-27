d3ec6ebccef08d7f4ee5ce57da4f66ff678c7199
dishanG09 <dishangkumar.patel@iiitb.ac.in>
2023-04-27 21:11:49 +0530

    logger configured

---
+++ backend/config/logger.js
@@ -0,0 +1,18 @@
+const { createLogger, format, transports, log } = require("winston");
+const winston = require("winston/lib/winston/config");
+const { combine, timestamp, printf } = format;
+
+const myFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  format: combine(timestamp(), myFormat),
+  transports: [
+    new transports.File({ filename: "./logs/server.log" }),
+    new transports.Console(),
+  ],
+  exitOnError: false,
+});
+
+module.exports = logger;
