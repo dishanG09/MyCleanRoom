c234dd387581b2fd6f2aec108ecb4bc5571eaa0c
dishanG09 <dishangkumar.patel@iiitb.ac.in>
2023-04-27 21:11:41 +0530

    logger configured

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,24 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+try {
+  mongoose.connect(process.env.DB_URI, {
+    autoIndex: false,
+    keepAlive: true,
+  });
+} catch (e) {
+  console.error(e);
+  throw new Error(e.message);
+}
+
+mongoose.connection.on("disconnected", (err, _) => {
+  if (err) {
+    console.error(err);
+    throw new Error(err);
+  }
+});
+
+mongoose.connection.on("error", (err, _) => {
+  console.error(err);
+  throw new Error(err);
+});
