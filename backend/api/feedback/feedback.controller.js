f518093050d9ba2df46f790acd587dd69cbf8902
dishanG09 <dishangkumar.patel@iiitb.ac.in>
2023-05-09 15:51:49 +0530

    [new code] : feedback analytics endpoint add

--- backend/api/feedback/feedback.controller.js
+++ backend/api/feedback/feedback.controller.js
@@ -2,7 +2,7 @@ const Router = require("express").Router;
 const validateFeedback = require("./feedback.validation");
 const router = Router();
 const Feedback = require("../feedback/feedback.model");
-const { Student, HKStaff } = require("../user/user.model");
+const { Student, HKStaff, Supervisor } = require("../user/user.model");
 const errors = require("../../utils/errosmessage");
 const { extractToken } = require("../../middleware/tokenParser");
 
@@ -49,4 +49,61 @@ router.post(
   }
 );
 
+router.get("/get-feedback/for-today", extractToken, async (req, res, next) => {
+  try {
+    // verify request is coming from valid supervisor
+
+    const supervisor = await Supervisor.findOne({
+      _id: { $eq: req.params["id"] },
+    });
+
+    if (!supervisor) throw new Error(errors.BAD_REQUEST);
+
+    // initializing date for comparision
+
+    const startDate = new Date();
+    startDate.setHours(0, 0, 0, 0);
+    const endDate = new Date();
+    endDate.setHours(23, 59, 59, 999);
+
+    const data = await Feedback.find({
+      createdAt: { $gte: startDate, $lt: endDate },
+    });
+
+    res.json(data);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get(
+  "/get-feedback/group-monthwise",
+  extractToken,
+  async (req, res, next) => {
+    try {
+      const initDate = new Date();
+      initDate.setHours(0, 0, 0, 0);
+      initDate.setMonth(0);
+      initDate.setDate(1);
+
+      const data = await Feedback.aggregate([
+        {
+          $match: {
+            createdAt: { $gt: initDate },
+          },
+        },
+        {
+          $group: {
+            _id: { $month: "$createdAt" },
+            count: { $sum: 1 },
+            avg_rating: { $avg: "$rating" },
+          },
+        },
+      ]);
+      res.json(data);
+    } catch (e) {
+      next(e);
+    }
+  }
+);
 module.exports = router;
